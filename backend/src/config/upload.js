3462c4536bcd62284e53a6bf4e04b2b6a077383a
Diego da Silva Lourenço <diego.silva@cfp.org.br>
2020-01-31 17:41:05 -0300

    Update: (Backend) Regex para o filename

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -5,9 +5,12 @@ module.exports = {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, "..", "..", "uploads"),
     filename: (request, file, callback) => {
-      const extension = path.extname(file.originalname);
-      const filename = path.basename(file.originalname, extension);
       const datetime = Date.now();
+      const extension = path.extname(file.originalname);
+      const filename = path.basename(
+        file.originalname.replace(/\W/g, ""),
+        extension
+      );
 
       callback(null, `${filename}-${datetime}${extension}`);
     }
