bfcd4c707c323f34a9f0275f1ca8ea216aa8d605
Diego da Silva Lourenço <diego.silva@cfp.org.br>
2020-02-05 15:57:03 -0300

    Update: (Mobile) SafeAreaView

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,35 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, AsyncStorage, Image, View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import logo from "../assets/logo.png";
 
 export default function List() {
-  return <View />;
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem("techs").then(storageTechs => {
+      const techsArray = storageTechs.split(", ");
+
+      setTechs(techsArray);
+    });
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 20
+  }
+});
